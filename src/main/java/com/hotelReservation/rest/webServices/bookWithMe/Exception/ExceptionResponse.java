6fcf472fd889244901a522b2248604062fee12f6
malinduruwantha <malinduruwantha@gmail.com>
2021-10-01 22:11:49 -0700

    added customized exception handler

--- src/main/java/com/hotelReservation/rest/webServices/bookWithMe/Exception/ExceptionResponse.java
+++ src/main/java/com/hotelReservation/rest/webServices/bookWithMe/Exception/ExceptionResponse.java
@@ -1,5 +1,33 @@
 package com.hotelReservation.rest.webServices.bookWithMe.Exception;
 
-public class ExceptionResponse {
+import java.util.Date;
 
+public class ExceptionResponse {
+	
+	private Date timestamp;
+	private String fields;
+	private String details;
+	
+	public ExceptionResponse(Date timestamp, String fields, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.fields = fields;
+		this.details = details;
+	}
+
+	
+
+	public Date getTimestamp() {
+		return timestamp;
+	}
+
+	public String getFields() {
+		return fields;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+	
+	
 }
